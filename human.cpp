8c4da85a36470135c3519589ec2f0c9ae5cf54fd
정은하 <97080242+jeongeunhq@users.noreply.github.com>
2023-11-28 13:58:25 +0900

    합침2

--- human.cpp
+++ human.cpp
@@ -2,7 +2,6 @@
 #include <gl/gl.h>
 #include <gl/glu.h>
 static int Dance = 0, a = 20, b = 20, c = 0, d = 0, e = 0, f = 0, g = 0, h = 0;
-
 void Draw_Body();
 void Draw_Neck();
 void Draw_Head();
@@ -25,13 +24,37 @@ void Draw_R_Foot();
 void drawFilledCylinder(GLfloat radius, GLfloat height, GLint slices, GLint stacks);
 //a= 왼팔, b=오른팔 c=왼 팔목 d=오른 팔목 e=왼 허벅지 f= 오른 허벅지 g=왼 발목 h=오른 발목
 
+void SetupLighting() {
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+
+	GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
+	GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+
+	GLfloat material_ambient[] = { 0.2, 0.2, 0.2, 1.0 };
+	GLfloat material_diffuse[] = { 0.0, 0.0, 1.0, 1.0 };
+	GLfloat material_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat material_shininess[] = { 100.0 };
+
+	glMaterialfv(GL_FRONT, GL_AMBIENT, material_ambient);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, material_diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, material_specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, material_shininess);
+}
+
 void MyDisplay() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluLookAt(0.0, 0.0, 3.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	gluLookAt(0.0, 1.0, 3.0, 0.0, 0.0, 0.0, 0.0, 4.0, 0.0);
 	glRotatef((GLfloat)Dance, 0.0, 1.0, 0.0);
 	glTranslatef(0.0, 0.31, 0.0);
+	SetupLighting();
 	Draw_Body();
 	glPushMatrix();
 	glTranslatef(0.0, 0.02, 0.0);
@@ -149,12 +172,25 @@ void MyDisplay() {
 	glPopMatrix(); //오른쪽 다리 시작 관절
 	glPopMatrix(); //몸통
 
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60.0, 4.0 / 3.0, 1.0, 15.0);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0, 7, 0, 0, 5, 0, 0, 0, -1);
+	
+	// 첫 번째 원기둥 그리기
+	glPushMatrix();
 	glColor3f(1.0, 1.0, 1.0);  // 흰색
 	drawFilledCylinder(2.0, 0.2, 20, 10);
-
-	// 두 번째 원기둥
-	glColor3f(1.0, 1.0, 1.0);  // 흰색
+	glPopMatrix();
+	gluLookAt(0, 7, 0, 0, 5, 0, 0, 0, -1);
+	// 두 번째 원기둥 그리기
+	glPushMatrix();
 	glTranslatef(0.0, 0.0, 0.2);
+	
+	glColor3f(1.0, 1.0, 1.0);  // 흰색
 	drawFilledCylinder(2.5, 0.4, 20, 10);
 	glPopMatrix();
 
@@ -242,7 +278,7 @@ void Draw_Body()
 	glColor3f(0.0, 0.0, 1.0);
 	// 몸통을 와이어프레임 구 형태로 그립니다.
 	// 반지름이 0.3이며, 가로 및 세로에 20개의 슬라이스 및 스택을 가진 구를 그립니다.
-	glutWireSphere(0.3, 100, 100);
+	glutSolidSphere(0.3, 100, 100);
 }
 
 void Draw_Neck()
@@ -252,7 +288,7 @@ void Draw_Neck()
 	glColor3f(1.0, 0.87, 0.68);
 	// 목을 와이어프레임 구 형태로 그립니다.
    // 반지름이 0.04이며, 가로 및 세로에 20개의 슬라이스 및 스택을 가진 구를 그립니다.
-	glutWireSphere(0.04, 80, 80);
+	glutSolidSphere(0.04, 80, 80);
 }
 
 void Draw_L_Ball()
@@ -263,38 +299,13 @@ void Draw_L_Ball()
 	glColor3f(0.0, 0.0, 1.0);
 	// 왼쪽 손목 부분을 와이어프레임 구 형태로 그립니다.
 	// 반지름이 0.04이며, 가로 및 세로에 20개의 슬라이스 및 스택을 가진 구를 그립니다.
-	glutWireSphere(0.04, 80, 80);
+	glutSolidSphere(0.04, 80, 80);
 }
 
-void Draw_Eyes() {
-	// 첫 번째 눈
-	glColor3f(0.0, 0.0, 0.0); // 검정색으로 설정
-	glPushMatrix();
-	glTranslatef(-0.1, 0.1, 2.5); // 눈의 위치 조절
-	glutSolidSphere(0.03, 20, 20);
-	glPopMatrix();
-
-	// 두 번째 눈
-	glPushMatrix();
-	glTranslatef(0.1, 0.1, 2.5); // 눈의 위치 조절
-	glutSolidSphere(0.03, 20, 20);
-	glPopMatrix();
-}
 
 void Draw_Head() {
 	glColor3f(1.0, 0.87, 0.68); // 머리의 기본 색상 설정
-	glutWireSphere(0.25, 90, 90); // 머리 모양 및 크기 설정
-
-	// 눈 그리기
-	glPushMatrix();
-
-	Draw_Eyes(); // 오른쪽 눈 그리기
-	glPopMatrix();
-
-	glPushMatrix();
-
-	Draw_Eyes(); // 왼쪽 눈 그리기
-	glPopMatrix();
+	glutSolidSphere(0.25, 90, 90); // 머리 모양 및 크기 설정
 
 }
 
@@ -307,7 +318,7 @@ void Draw_L_UpperArm()
 	glColor3f(0.0, 0.0, 1.0);
 	// 왼쪽 상단 팔 부분을 와이어프레임 구 형태로 그립니다.
 	// 반지름이 0.15이며, 가로 및 세로에 20개의 슬라이스 및 스택을 가진 구를 그립니다.
-	glutWireSphere(0.15, 80, 80);
+	glutSolidSphere(0.15, 80, 80);
 }
 
 void Draw_L_LowerArm()
@@ -318,7 +329,7 @@ void Draw_L_LowerArm()
 	glColor3f(0.0, 0.0, 1.0);
 	// 왼쪽 하단 팔 부분(팔목)을 와이어프레임 구 형태로 그립니다.
 	// 반지름이 0.2이며, 가로 및 세로에 20개의 슬라이스 및 스택을 가진 구를 그립니다.
-	glutWireSphere(0.2, 80, 80);
+	glutSolidSphere(0.2, 80, 80);
 }
 
 void Draw_L_Hand()
@@ -329,7 +340,7 @@ void Draw_L_Hand()
 	glColor3f(1.0, 0.87, 0.68);
 	// 왼쪽 손을 와이어프레임 구 형태로 그립니다.
 	// 반지름이 0.1이며, 가로 및 세로에 20개의 슬라이스 및 스택을 가진 구를 그립니다.
-	glutWireSphere(0.1, 80, 80);
+	glutSolidSphere(0.1, 80, 80);
 }
 
 void Draw_R_Ball()
@@ -339,7 +350,7 @@ void Draw_R_Ball()
 	glColor3f(0.0, 0.0, 1.0);
 	// 오른쪽 손목 부분을 와이어프레임 구 형태로 그립니다.
 // 반지름이 0.04이며, 가로 및 세로에 20개의 슬라이스 및 스택을 가진 구를 그립니다.
-	glutWireSphere(0.04, 80, 80);
+	glutSolidSphere(0.04, 80, 80);
 }
 
 void Draw_R_UpperArm()
@@ -349,7 +360,7 @@ void Draw_R_UpperArm()
 	glColor3f(0.0, 0.0, 1.0);
 	// 오른쪽 상단 팔 부분을 와이어프레임 구 형태로 그립니다.
 	// 반지름이 0.15이며, 가로 및 세로에 20개의 슬라이스 및 스택을 가진 구를 그립니다.
-	glutWireSphere(0.15, 80, 80);
+	glutSolidSphere(0.15, 80, 80);
 }
 
 void Draw_R_LowerArm()
@@ -359,7 +370,7 @@ void Draw_R_LowerArm()
 	glColor3f(0.0, 0.0, 1.0);
 	// 오른쪽 하단 팔 부분(팔목)을 와이어프레임 구 형태로 그립니다.
 	// 반지름이 0.2이며, 가로 및 세로에 20개의 슬라이스 및 스택을 가진 구를 그립니다.
-	glutWireSphere(0.2, 80, 80);
+	glutSolidSphere(0.2, 80, 80);
 }
 
 void Draw_R_Hand() {
@@ -371,7 +382,7 @@ void Draw_R_Hand() {
 
 	// 오른쪽 손을 와이어프레임 구 형태로 그립니다.
 	// 반지름이 0.1이며, 가로 및 세로에 20개의 슬라이스 및 스택을 가진 구를 그립니다.
-	glutWireSphere(0.1, 80, 80);
+	glutSolidSphere(0.1, 80, 80);
 }
 
 void Draw_L_U_Ball() {
@@ -383,7 +394,7 @@ void Draw_L_U_Ball() {
 
 	// 왼쪽 상단 다리의 시작 부분을 와이어프레임 구 형태로 그립니다.
 	// 반지름이 0.04이며, 가로 및 세로에 20개의 슬라이스 및 스택을 가진 구를 그립니다.
-	glutWireSphere(0.04, 80, 80);
+	glutSolidSphere(0.04, 80, 80);
 }
 
 void Draw_L_UpperLeg() {
@@ -395,7 +406,7 @@ void Draw_L_UpperLeg() {
 
 	// 왼쪽 허벅지 부분을 와이어프레임 구 형태로 그립니다.
 	// 반지름이 0.2이며, 가로 및 세로에 20개의 슬라이스 및 스택을 가진 구를 그립니다.
-	glutWireSphere(0.2, 80, 80);
+	glutSolidSphere(0.2, 80, 80);
 }
 
 void Draw_L_LowerLeg() {
@@ -407,7 +418,7 @@ void Draw_L_LowerLeg() {
 
 	// 왼쪽 아래쪽 다리 부분을 와이어프레임 구 형태로 그립니다.
 	// 반지름이 0.15이며, 가로 및 세로에 20개의 슬라이스 및 스택을 가진 구를 그립니다.
-	glutWireSphere(0.15, 80, 80);
+	glutSolidSphere(0.15, 80, 80);
 }
 
 void Draw_L_Foot() {
@@ -419,7 +430,7 @@ void Draw_L_Foot() {
 
 	// 왼쪽 발 부분을 와이어프레임 구 형태로 그립니다.
 	// 반지름이 0.1이며, 가로 및 세로에 20개의 슬라이스 및 스택을 가진 구를 그립니다.
-	glutWireSphere(0.1, 80, 80);
+	glutSolidSphere(0.1, 80, 80);
 }
 
 
@@ -432,7 +443,7 @@ void Draw_R_U_Ball() {
 
 	// 오른쪽 다리의 시작 부분 관절을 와이어프레임 구 형태로 그립니다.
 	// 반지름이 0.04이며, 가로 및 세로에 20개의 슬라이스 및 스택을 가진 구를 그립니다.
-	glutWireSphere(0.04, 80, 80);
+	glutSolidSphere(0.04, 80, 80);
 }
 
 
@@ -445,7 +456,7 @@ void Draw_R_UpperLeg() {
 
 	// 오른쪽 허벅지 부분을 와이어프레임 구 형태로 그립니다.
 	// 반지름이 0.2이며, 가로 및 세로에 20개의 슬라이스 및 스택을 가진 구를 그립니다.
-	glutWireSphere(0.2, 80, 80);
+	glutSolidSphere(0.2, 80, 80);
 }
 
 
@@ -458,7 +469,7 @@ void Draw_R_LowerLeg() {
 
 	// 오른쪽 종아리 부분을 와이어프레임 구 형태로 그립니다.
 	// 반지름이 0.15이며, 가로 및 세로에 20개의 슬라이스 및 스택을 가진 구를 그립니다.
-	glutWireSphere(0.15, 80, 80);
+	glutSolidSphere(0.15, 80, 80);
 }
 
 
@@ -471,7 +482,7 @@ void Draw_R_Foot() {
 
 	// 오른쪽 발 부분을 와이어프레임 구 형태로 그립니다.
 	// 반지름이 0.1이며, 가로 및 세로에 20개의 슬라이스 및 스택을 가진 구를 그립니다.
-	glutWireSphere(0.1, 80, 80);
+	glutSolidSphere(0.1, 80, 80);
 }
 
 
@@ -481,7 +492,7 @@ int main(int argc, char** argv) {
 	glutInitWindowSize(1280, 980);
 	glutInitWindowPosition(0, 0);
 	glutCreateWindow("human");
-	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClearColor(0.5, 0.5, 0.5, 1.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(45.0, 1.0, 0.1, 100.0);
